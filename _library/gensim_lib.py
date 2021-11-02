4ae2298f31a2faa163c6a4cb91f90cdcfc6ab353
Koji Wajima <kwajima@ce.slis.tsukuba.ac.jp>
2021-11-03 01:49:52 +0900

    Add files via upload

---
+++ _library/gensim_lib.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+
+import gensim
+from gensim import corpora, models, similarities
+from gensim.models import doc2vec
+
+######    コーパス・辞書    ######
+##トークンからGensimの辞書を作成##
+def tokens_To_GensimDic(_tokens):
+	dictionary = corpora.Dictionary(_tokens)
+	return dictionary
+
+##トークンからGensimのコーパスを作成##
+def tokens_To_GensimCorpus(_tokens,_dictionary):
+	corpus = [_dictionary.doc2bow(document) for document in _tokens]
+	return corpus
+
+##Gensimの辞書を指定ディレクトリに保存##
+def store_The_GensimDic(_dictionary,_path):
+	_dictionary.save(_path)
+
+##Gensimの辞書を指定ディレクトリから読込##
+def load_The_GensimDic(_path):
+	dictionary = gensim.corpora.dictionary.Dictionary.load(_path)
+	return dictionary
+
+##Gensimのコーパスを指定ディレクトリに保存##
+def store_The_GensimCorpus(_corpus,_path):
+	corpora.MmCorpus.serialize(_path,_corpus)
+
+##Gensimのコーパスを指定ディレクトリから読込##
+def load_The_GensimCorpus(_path):
+	corpus = gensim.corpora.MmCorpus(_path)
+	return corpus
+
+##GensimのコーパスをTFIDFモデルを構築##
+def gensimCorpus_To_TfidfInstance(_corpus):
+	tfidf_instance=models.TfidfModel(_corpus)
+	return tfidf_instance
+
+##GensimのコーパスをTFIDFモデルのコーパスに変換##
+def gensimCorpus_To_TfidfCorpus(_corpus,_tfidf_instance):
+	tfidf_corpus = _tfidf_instance[_corpus]
+	return tfidf_corpus
+
+######    LDA    ######
+##LDAトピックの抽出##
+def extraction_LdaTopic(_dictionary,_corpus,_num_topics):
+	lda_Topic = gensim.models.ldamodel.LdaModel(corpus=_corpus, num_topics=_num_topics, id2word=_dictionary)
+	return lda_Topic
+
+##LDAトピックの保存##
+def store_The_LdaTopic(_lda_Topic,_path):
+	_lda_Topic.save(_path)
+
+##LDAトピックの読込##
+def load_The_LdaTopic(_path):
+	lda_Topic = gensim.models.LdaModel.load(_path)
+	return lda_Topic
+
+##LDAトピックの割当##
+def doc_to_ldatopic_distribution(_text,_ldatopic):
+	doc_lda = _ldatopic[_text]
+	return doc_lda
+
+
+######    LSI    ######
+##LSIトピックの抽出##
+def extraction_LsiTopic(_dictionary,_tfidf_corpus,_num_topics):
+	lsi_Topic = models.LsiModel(_tfidf_corpus, id2word=_dictionary, num_topics=_num_topics)
+	return lsi_Topic
+
+##LSIトピックの保存##
+def store_The_LsiTopic(_lsi_Topic,_path):
+	_lsi_Topic.save(_path)
+
+##LSIトピックの読込##
+def load_The_LsiTopic(_path):
+	lsi_Topic = gensim.models.LsiModel.load(_path)
+	return lsi_Topic
+
+##LSIトピックの割当##
+def doc_to_lsitopic_distribution(_text,_lsitopic):
+	doc_lsi = _lsitopic[_text]
+	return doc_lsi
+
+######    HDP    ######
+##HDPトピックの抽出##
+def extraction_HdpTopic(_dictionary,_corpus):
+	hdp_Topic = models.HdpModel(_corpus, id2word=_dictionary)
+	return hdp_Topic
+
+##HDPトピックの保存##
+def store_The_HdpTopic(_hdp_Topic,_path):
+	_hdp_Topic.save(_path)
+
+##HDPトピックの読込##
+def load_The_HdpTopic(_path):
+	hdp_Topic = gensim.models.HdpModel.load(_path)
+	return hdp_Topic
+
+##HDPトピックの割当##
+def doc_to_hdptopic_distribution(_text,_hdptopic):
+	doc_hdp = _hdptopic[_text]
+	return doc_hdp
+
+######    LSIにおける類似度    ######
+##入力クエリをLSIのベクトル空間に変換##
+def lsi_convert_queries(_dictionary,_lsi_Topic,_text):
+	vec_bow = _dictionary.doc2bow(_text.split())
+	vec_lsi = _lsi_Topic[vec_bow]
+	return vec_lsi
+	
+##コーパスをLSIのベクトル空間に変換##
+def lsi_convert_corpus(_corpus,_lsi_Topic):
+	index = similarities.MatrixSimilarity(_lsi_Topic[_corpus])
+	return index
+	
+##LSIのベクトル空間を保存##
+def store_The_lsi_corpus(_index,_path):
+	_index.save(_path)
+	
+##LSIのベクトル空間を読込##
+def load_The_lsi_corpus(_path):
+	index = similarities.MatrixSimilarity.load(_path)
+	return index
+	
+##コーパスと入力クエリとのLSIのベクトル空間上の類似度を算出##
+def lsi_similarity_calculation(_vec_lsi,_index):
+	sims = _index[_vec_lsi]
+	sims = sorted(enumerate(sims), key=lambda item: -item[1])
+	return sims
+	
+######    Word2Vec    ######
+##Word2Vecインスタンス作成##
+def tokens_To_Word2Vec(_wakachi_path):
+	
+	#分かち書き済みのテキストファイルを読込
+	sentences = models.word2vec.LineSentence(_wakachi_path)
+	
+	#Word2Vecのインスタンス作成
+	model = models.word2vec.Word2Vec(sentences, size=200, min_count=20, window=15)
+	
+	return model
+	
+##Word2Vecモデルの保存##
+def store_The_Word2Vec(_model,_path):
+	_model.save(_path)
+	
+##Word2Vecモデルの読込##
+def load_The_Word2Vec(_path):
+	model = models.word2vec.Word2Vec.load(_path)
+	return model
+
+##特定の単語に対する類似度上位Nの単語を返却##
+def word_To_Word2Vec_list(_word,_model,_wordNum):
+	
+	try:
+		result = _model.most_similar(unicode(_word, 'utf-8'))
+		#result = _model.most_similar(positive=_word,topn=_wordNum)
+		
+	except KeyError:
+		#print "## not in vocabulary(under min_count) ##"
+		result = ["\t\t","\t\t"]
+		
+	return result
+
+######    Doc2Vec    ######
+##Doc2Vecインスタンス作成##
+def tokens_To_Doc2Vec(_wakachi_path):
+	
+	print _wakachi_path
+	
+	#分かち書き済みのテキストファイルを読込
+	sentences = models.doc2vec.TaggedLineDocument(_wakachi_path)
+	
+	#Word2Vecのインスタンス作成
+	model = models.doc2vec.Doc2Vec(sentences, size=300, min_count=20, window=15)
+	
+	return model
+	
+##Doc2Vecモデルの保存##
+def store_The_Doc2Vec(_model,_path):
+	model.save(_path)
+	
+##Doc2Vecモデルの読込##
+def load_The_Doc2Vec(_path):
+	model = models.doc2vec.Doc2Vec.load(_path)
+	return model
+
+##ある文書と類似度が近い文書群を返却##
+def doc2Vec_similar_list(_doc,_model,_docsNum):
+	result = _model.docvecs.most_similar(_doc,topn=_docsNum)
+	return result
+
+##2つの文書の類似度を算出##
+def doc2Vec_similarity_calculation(_doc1,_doc2,_model):
+	result = _model.docvecs.similarity(_doc1,_doc2)
+	return result
---
+++ _library/mecab_lib.py
@@ -0,0 +1,449 @@
+# -*- coding: utf-8 -*-
+import user_stopword
+import unicodedata
+import re
+import MeCab
+
+#MeCab
+mecab = MeCab.Tagger('-Ochasen')
+
+###各フィルターで使用する正規表現のコンパイル###
+
+#PIC
+pic_p = re.compile(u"[^\s]+(?=\.(jpg|jpeg|gif|png))\.\2")
+#Mail
+mail_p = re.compile(u"[\w\d_-]+@[\w\d_-]+\.[\w\d._-]+")
+#HTML
+html_p = re.compile(u"\<(/?[^\>]+)\>")
+#ColorCode
+colorcode_p = re.compile(u"'#'?([A-Fa-f0-9]){3}(([A-Fa-f0-9]){3})?")
+#URL
+url_p = re.compile(u"(?:https?|ftp):\/\/[\w\d/%#$&?()~_.=+-]+")
+#symbol
+symbol_p = re.compile(u"[!-/:-@[-`{-~]")
+
+#number
+number_p = re.compile(u"([0-9]+)")
+
+#english
+english_p = re.compile(u"([A-Za-z0-9]+)")
+
+#ひらがな2文字
+hiragana_p = re.compile(u"[ぁ-ゞ]{2}")
+
+#カタカナ末尾長音記号
+katakan_p = re.compile(u"$ー")
+
+#トークン化
+def text_ja(_text,_posType,_formType):
+	wordList = []
+	
+	#改行コードを除去
+	text = _text.strip()
+	
+	#unicode正規化
+	CJKText = CJKWidthFilterFactory(_text)
+	
+	#小文字化
+	LCText = LowerCaseFilterFactory(CJKText)
+	
+	#クレンジング
+	RDCText = RegularDataCleansing(LCText)
+	
+	#トークン化
+	mecabTokens = Tokens(RDCText)
+	
+	#品詞フィルター
+	posTokens = JapanesePartOfSpeechStopFilterFactory(mecabTokens,_posType)
+	
+	#文字数フィルター
+	WLTokens = WordLengthFilterFactory(posTokens)
+	
+	#活用形フィルター + リスト化
+	textWords = JapaneseFormFilterFactory(WLTokens,_formType)
+	
+	#長音記号フィルター
+	JKSWords = JapaneseKatakanaStemFilterFactory(textWords)
+	
+	#アルファベットフィルター
+	#nonEnglishWords = englishFilterFactory(JKSWords)
+	
+	#記号フィルター
+	nonSymbolWords = symbolFilterFactory(JKSWords)
+	
+	#STOP WORDフィルター
+	JSWords = JapaneseStopFilterFactory(nonSymbolWords)
+	
+	#nonNumberWords =  numberFilterFactory(nonSymbolWords)
+	nonNumberWords =  numberFilterFactory(JSWords)
+	
+	#unicode正規化
+	#CJKTokens = CJKTokenFilterFactory(JSWords)
+	
+	return nonNumberWords
+
+#品詞カウント用
+def posTagger(_text,_pos_list):
+	try:
+		wordList = []
+		
+		#改行コードを除去
+		text = _text.strip()
+		
+		#unicode正規化
+		CJKText = CJKWidthFilterFactory(_text)
+		
+		#小文字化
+		LCText = LowerCaseFilterFactory(CJKText)
+		
+		#クレンジング
+		RDCText = RegularDataCleansing(LCText)
+		
+		#形態素解析
+		words = Tokens(RDCText)
+		
+		#品詞情報のみ取得
+		tag_Lists = classifyPos_tag(words,_pos_list)
+		
+		return tag_Lists
+		
+	except Exception as e:
+		print e
+
+#品詞情報を取得するメソッド
+def classifyPos_tag(_words,_pos_list):
+	tags = []
+	for token in _words.tokens:
+		try :
+			if token.pos in _pos_list:
+				tags.append(token.pos)
+		except Exception as e:
+			print e
+			
+	return tags
+
+#pos_detail1カウント用
+def detail1Tagger(_text,_detail1_list):
+	try:
+		wordList = []
+		
+		#改行コードを除去
+		text = _text.strip()
+		
+		#unicode正規化
+		CJKText = CJKWidthFilterFactory(_text)
+		
+		#小文字化
+		LCText = LowerCaseFilterFactory(CJKText)
+		
+		#クレンジング
+		RDCText = RegularDataCleansing(LCText)
+		
+		#形態素解析
+		words = Tokens(RDCText)
+		
+		#品詞情報のみ取得
+		detail1_Lists = detail1_tag(words,_detail1_list)
+		
+		return detail1_Lists
+		
+	except Exception as e:
+		print e
+
+#pos_detail1を取得するメソッド
+def detail1_tag(_words,_detail1_list):
+	tags = []
+	for token in _words.tokens:
+		try :
+			if token.pos_detail1 in _detail1_list:
+				tags.append(unicode(token.pos_detail1))
+				#tags.append(token.pos_detail1)
+		except Exception as e:
+			print e
+			
+	return tags
+
+
+#Subtypeカウント用
+def subtypeTagger(_text,_subtype_list):
+	try:
+		wordList = []
+		
+		#改行コードを除去
+		text = _text.strip()
+		
+		#unicode正規化
+		CJKText = CJKWidthFilterFactory(_text)
+		
+		#小文字化
+		LCText = LowerCaseFilterFactory(CJKText)
+		
+		#クレンジング
+		RDCText = RegularDataCleansing(LCText)
+		
+		#形態素解析
+		words = Tokens(RDCText)
+		
+		#品詞情報のみ取得
+		subtype_Lists = subtype_tag(words,_subtype_list)
+		
+		return subtype_Lists
+		
+	except Exception as e:
+		print e
+
+#Subtypeを取得するメソッド
+def subtype_tag(_words,_subtype_list):
+	tags = []
+	for token in _words.tokens:
+		try :
+			if token.pos_detail2 in _subtype_list:
+				tags.append(unicode(token.pos_detail2))
+				#tags.append(token.pos_detail2)
+		except Exception as e:
+			print e
+			
+	return tags
+
+#twitterのアカウントを落とす用
+def englishFilterFactory(_tokens):
+
+        nonEnglishTokens = []
+        for token in _tokens:
+                try :
+			if not re.match(english_p,unicode(token,'utf-8')):
+				nonEnglishTokens.append(token)
+                except Exception as e:
+                        print e
+
+        return nonEnglishTokens
+
+#数字を落とす用
+def numberFilterFactory(_tokens):
+        nonNumberTokens = []
+        for token in _tokens:
+                try :
+                        if not re.match(number_p,unicode(token,'utf-8')):
+                                nonNumberTokens.append(token)
+                except Exception as e:
+                        print e
+
+        return nonNumberTokens
+
+def symbolFilterFactory(_tokens):
+	
+	nonSymbolTokens = []
+	for token in _tokens:
+                try :
+                        if not re.match(symbol_p,unicode(token,'utf-8')):
+                                nonSymbolTokens.append(token)
+                except Exception as e:
+                        print e
+
+	return nonSymbolTokens
+
+#unicode正規化
+def CJKTokenFilterFactory(_tokens):
+	CJKTokens = []
+	for token in _tokens:
+                try :
+                	unicode_token = unicode(token)
+			nfkc_token = unicodedata.normalize('NFKC', unicode_token)
+			CJKToken = nfkc_token.encode('utf-8')
+			CJKTokens.append(CJKToken)
+		except Exception as e:
+                        print e
+        return CJKTokens
+
+#Unicode正規化
+def CJKWidthFilterFactory(_text):
+	#改行コードの除去
+	#text = _text.rstrip()
+	
+	try :
+		#Unicode正規化
+		unicode_text = unicode(_text)
+		nfkc_text = unicodedata.normalize('NFKC', unicode_text)
+		CJKText = nfkc_text.encode('utf-8')
+	except Exception as e:
+		CJKText = ""
+		print e
+		
+	return CJKText
+
+#英数字の小文字化
+def LowerCaseFilterFactory(_CJKText):
+	LCText = _CJKText.lower()
+	return LCText
+
+#正規表現によるクレンジング
+def RegularDataCleansing(_LCText):
+	
+	#unicodeにデコード
+	LCText = _LCText.decode('utf-8')
+	
+	#URL
+	s_result = url_p.search(LCText)
+	urlText = LCText
+	while s_result is not None:
+		urlText = urlText.replace(s_result.group(0),u" ")
+		#print s_result.group(0)
+		s_result = url_p.search(urlText)
+	
+	#PIC
+	s_result = pic_p.search(urlText)
+	picText = urlText
+        while s_result is not None:
+                picText = picText.replace(s_result.group(0),u" ")
+		s_result = url_p.search(picText)
+		
+	#Mail
+	s_result = mail_p.search(picText)
+        mailText = picText
+	while s_result is not None:
+                mailText = mailText.replace(s_result.group(0),u" ")
+		#print s_result.group(0)
+		s_result = mail_p.search(mailText)
+	
+	#html
+	s_result = html_p.search(mailText)
+        htmlText = mailText
+	while s_result is not None:
+                htmlText = mailText.replace(s_result.group(0),u" ")
+		#print s_result.group(0)
+		s_result = mail_p.search(htmlText)
+			
+	#COLOR CODE
+	s_result = colorcode_p.search(htmlText)
+	colorcodeText = htmlText
+        while s_result is not None:
+                colorcodeText = htmlText.replace(s_result.group(0),u" ")
+		s_result = colorcode_p.search(colorcodeText)
+	
+	#記号
+	s_result = symbol_p.search(colorcodeText)
+	symbolText = colorcodeText
+        while s_result is not None:
+                symbolText = colorcodeText.replace(s_result.group(0),u" ")
+		s_result = colorcode_p.search(symbolText)
+	
+	
+	#数字
+	s_result = number_p.search(symbolText)
+	numberText = symbolText
+        while s_result is not None:
+                numberText = symbolText.replace(s_result.group(0),u" ")
+		#print s_result.group(0)
+		s_result = colorcode_p.search(numberText)
+	
+	#utf-8にエンコード	
+	RDText = mailText.encode('utf-8')
+	return RDText
+	
+#MeCabで単語をToken化
+def mecabTokenizer(_text):
+        mecabTokens = Tokens(_text)
+        return mecabTokens
+
+#品詞フィルタ
+def JapanesePartOfSpeechStopFilterFactory(_mecabTokens,_posType):
+	posTokens = []
+	#単語ごとにFor
+	for mecabToken in _mecabTokens.tokens:
+		#品詞が指定されたリストに含まれる場合
+		if mecabToken.pos in _posType:
+			#要素をリストに追加
+			posTokens.append(mecabToken)
+	return posTokens
+
+#文字数での除去(1文字除去、ひらがな2文字除去)
+def WordLengthFilterFactory(_posTokens):
+	WLTokens = []
+	for token in _posTokens:
+		try :
+			#1文字単語を除去
+			if len(unicode(token.surface,'utf-8')) == 1:continue
+			elif len(unicode(token.surface,'utf-8')) == 2:
+				#ひらがな2文字は除去
+				if re.match(hiragana_p,unicode(token.surface,'utf-8')):
+					continue
+				else:
+					WLTokens.append(token)
+			else:
+				WLTokens.append(token)
+		except Exception as e:
+			print e	
+	return WLTokens
+
+#活用形のフィルタ
+def JapaneseFormFilterFactory(_WLTokens,_formType):
+	textWords = []
+	for token in _WLTokens:
+		try:
+			if _formType == "basic":
+				if token.basic == "*":
+					textWords.append(token.surface)
+				else:
+					textWords.append(token.basic)
+			else:
+				JFTokens.surface(token.surface)
+		except Exception as e:
+			print e
+	return textWords
+
+#末尾の長音記号を除去
+def JapaneseKatakanaStemFilterFactory(_textWords):
+		JKSWords = []
+		for word in _textWords:
+			try:
+				re.sub(katakan_p,unicode(word,'utf-8'),"")
+				JKSWords.append(word)
+			except Exception as e:
+				print e
+		return JKSWords
+		
+#STOP WORDフィルタ
+def JapaneseStopFilterFactory(_JKSWords):
+	JSWords = []
+	for word in _JKSWords:
+		try:
+			if unicode(word,'utf-8') in user_stopword.User_stopword.user_stop_utf:
+				continue
+			else:
+				#print word
+				JSWords.append(word)
+		except Exception as e:
+				print e
+	return JSWords
+
+
+class Tokens(object) :
+        # text の形態素情報を保持する
+        def __init__ (self,_text):
+                self.text = _text
+                node = mecab.parseToNode(_text)
+                self.tokens = []
+                while node:
+                        self.tokens.append(Token(node.surface,*node.feature.split(',')))
+                        node = node.next
+
+class Token(object):
+        # 形態素の情報
+        def __init__(self,_surface,*_args):
+                #Mecab Format
+                #表層形/t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+                self.surface = _surface #表層情報
+
+                try:
+                        self.pos = _args[0] # 品詞
+                        self.pos_detail1 = _args[1] #品詞分類1
+                        self.pos_detail2 = _args[2] #品詞分類2
+                        self.pos_detail3 = _args[3] #品詞分類3
+                        self.verb_form = _args[4] #活用形
+                        self.verb_type = _args[5] #活用型
+                        self.basic = _args[6] #原型
+                        self.reading = _args[7] #読み
+                        self.pronunciation = _args[8] #発音
+                        self.type = True #すべての要素が格納できた時のFlg
+                except IndexError:
+                        self.type = False #すべての要素が格納できなかった時のFlg
